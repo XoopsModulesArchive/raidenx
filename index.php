b420e2364684362d23881d0158e1adeab9d787ce
mambax7 <mambax7@gmail.com>
2020-07-11 22:47:55 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include("../../mainfile.php");
+$xoopsOption['template_main'] = "raidenx.html";
+include(XOOPS_ROOT_PATH."/header.php");
+include("header.php");
+
+$xoopsOption['show_rblock'] = 1;
+ ?>
+<html>
+<body>
+<p>
+<a href='http://crazymonkeygames.com/'>Special Thanks to Crazy Monkey Games</a>
+<p>
+<a href='http://westech.us/raidenx/index.html'>Raiden X created by Go0gley</a>
+<p>
+<a href="http://shadowsbastion.com/">Ported to XOOPS by Shadow's Bastion</a>
+</body>
+</html>
+
+
+<?php
+include(XOOPS_ROOT_PATH."/footer.php");
+?>
---
+++ xoops_version.php
@@ -0,0 +1,17 @@
+<?php
+$modversion['name'] = "RaidenX";
+$modversion['version'] = 1.00;
+$modversion['description'] = "Flash games hosted on your site.";
+$modversion['author'] = "Ported To Xoops By Shadow (<a href='http://shadowsbastion.com/'>Shadow's Bastion</a>)";
+$modversion['credits'] = "Raiden X created by Go0gle(<a href='http://westech.us/raidenx/index.html'>Raiden X created by Go0gley</a>)";
+$modversion['help'] = "";
+$modversion['license'] = "GPL see LICENSE";
+$modversion['official'] = 0;
+$modversion['image'] = "images/raidenx.jpg";
+$modversion['dirname'] = "raidenx";
+$modversion['hasAdmin'] = 0;
+$modversion['adminpath'] = "";
+$modversion['hasMain'] = 1;
+$modversion['templates'][1]['file'] = "raidenx.html";
+$modversion['templates'][1]['description'] = "The template file of index";
+?>
